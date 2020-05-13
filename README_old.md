9047d46acd19a9df3c63527901498ed623fd0b0e
jmaquin0 <juan.m.alvarez@gmail.com>
2020-05-13 18:55:57 -0500

    add travis.yml

--- README_old.md
+++
@@ -1,2 +0,0 @@
-# SD2020A-final-project
-Repository to develop final project
---
+++ test/ejemplo.test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+describe('Pruebas de ejemplo', () => {
+ it('debería retornar 2', () => {
+        assert.equal(1 + 1, 2);
+    });
+ it('Debería retornar 9', () => {
+        assert.equal(3 * 3, 9);
+    });
+});
